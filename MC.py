2ecac43233df331e4de144bd605f3a638389bc0b
RimerMcquvue <anshul.229309232@muj.manipal.edu>
2023-04-08 00:40:14 +0530

    Monter Carlo for finance

--- MC.py
+++ MC.py
@@ -3,13 +3,15 @@ import numpy as np
 import datetime as dt
 from pandas_datareader import data as pdr
 import yfinance as yf
-import matplotlib as mp
+import matplotlib.pyplot as plt
 # import data
 def get_data(stocks, start, end):
     # stockData = pdr.get_data_yahoo(stocks, start, end)
     stockData = yf.download(stocks, start,end)
     stockData = stockData['Close']
+    print(stockData)
     returns = stockData.pct_change()
+    print(returns)
     meanReturns = returns.mean()
     covMatrix = returns.cov()
     return meanReturns, covMatrix
@@ -22,5 +24,21 @@ print(stocks)
 print(startDate)
 print(endDate)
 meanReturns, covMatrix = get_data(stocks, startDate, endDate)
-
 print(meanReturns)
+
+weights=np.random.random(len(meanReturns))
+weights /=np.sum(weights)
+print(weights)
+
+#monte carlo method
+mc_sims=100#number of simulations
+T=100#timeframe in days
+
+meanM=np.full(shape=(T,len(weights)), fill_value=meanReturns)
+print(meanM)
+meanM=meanM.T
+
+portfolio_sims=np.full(shape=(T,mc_sims),fill_value=0.0)
+
+for m in range(0,mc_sims):
+    
